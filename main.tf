ff7468b5cc8efc9b6a4ed562b617c21f2878dc32
David Prangnell <david@prangnell.biz>
2023-08-18 12:04:27 -0700

    Chapter_4_1: Use module for vpc

--- main.tf
+++ main.tf
@@ -34,11 +34,12 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  subnet_id     = module.vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "5.1.0"
   name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id  = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
