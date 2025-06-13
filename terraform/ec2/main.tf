a41312720cdedacc63f4ddcb21d7447d525dc197
David Prangnell <david@prangnell.biz>
2025-06-13 15:51:20 -0700

    02_05 - intial commit

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count         = var.instance_count
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags   = local.combined_tags
 }
